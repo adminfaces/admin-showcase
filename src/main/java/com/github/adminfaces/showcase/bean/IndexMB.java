7896344dd9261b2f10b8277b27ffc7427ccb8243
rmpestano <bigboss666>
2017-01-08 09:45:02 -0200

    Initial drop in

---
+++ src/main/java/com/github/adminfaces/showcase/bean/IndexMB.java
@@ -0,0 +1,15 @@
+package com.github.adminfaces.showcase.bean;
+
+import org.omnifaces.cdi.ViewScoped;
+
+import javax.inject.Named;
+import java.io.Serializable;
+
+/**
+ * Created by rmpestano on 07/01/17.
+ */
+@Named
+@ViewScoped
+public class IndexMB implements Serializable {
+
+}
