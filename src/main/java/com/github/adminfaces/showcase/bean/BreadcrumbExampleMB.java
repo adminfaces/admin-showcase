4f9bbc0cf3e989508367313e02affb087a907c0a
rmpestano <bigboss666>
2017-01-22 01:53:51 -0200

    - adds sourceCode bean to include sources in examples - adds breadcrumb example

---
+++ src/main/java/com/github/adminfaces/showcase/bean/BreadcrumbExampleMB.java
@@ -0,0 +1,42 @@
+package com.github.adminfaces.showcase.bean;
+
+import com.github.adminfaces.template.bean.BreadCrumbMB;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+/**
+ * Created by rmpestano on 22/01/17.
+ */
+@Named
+@SessionScoped
+public class BreadcrumbExampleMB implements Serializable{
+
+    private String link;
+    private String title;
+
+    @Inject
+    private BreadCrumbMB breadCrumbMB;
+
+    public void add(){
+        breadCrumbMB.add(link,title);
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
