24d5ed7a4a410c270bef49a53aace7f8501aeb89
rmpestano <rmpestano@gmail.com>
2017-05-06 21:10:27 -0300

    perf

--- src/main/java/com/github/adminfaces/showcase/analytics/store/PageStatisticsStore.java
+++ src/main/java/com/github/adminfaces/showcase/analytics/store/PageStatisticsStore.java
@@ -28,7 +28,7 @@ public class PageStatisticsStore implements Serializable {
 
     private Map<String, PageStats> pageStatisticsMap; //viewId by statistics map
     private static final Logger log = LoggerFactory.getLogger(PageStatisticsStore.class.getName());
-    private final String pagesStatsFilePath = (System.getenv("OPENSHIFT_DATA_DIR") != null ? (System.getenv("OPENSHIFT_DATA_DIR") : System.getProperty("user.home") + "/page-stats.json").replaceAll("//", "/");
+    private final String pagesStatsFilePath = (System.getenv("OPENSHIFT_DATA_DIR") != null ? System.getenv("OPENSHIFT_DATA_DIR") : System.getProperty("user.home") + "/page-stats.json").replaceAll("//", "/");
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     @PostConstruct
