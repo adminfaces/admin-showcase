f387c51c03debf69ced419d05bc49ef02e9f1426
rmpestano <rmpestano@gmail.com>
2017-05-15 16:37:34 -0300

    avoid ip api ban

--- src/main/java/com/github/adminfaces/showcase/analytics/store/PageStatisticsStore.java
+++ src/main/java/com/github/adminfaces/showcase/analytics/store/PageStatisticsStore.java
@@ -219,7 +219,7 @@ public class PageStatisticsStore implements Serializable {
                 pageView.setLat(jsonObject.getJsonNumber("lat").toString());
                 pageView.setLon(jsonObject.getJsonNumber("lon").toString());
                 pageView.setHasIpInfo(true);
-                Thread.sleep(200);//sleep to not exceed query limits (150 per minute)
+                Thread.sleep(250);//sleep to not exceed query limits (150 per minute)
                 return true;
             }
 
