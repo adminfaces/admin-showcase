3c297e9fd6d40f769da31cad17e3215e468cb743
rmpestano <rmpestano@gmail.com>
2017-05-24 22:38:52 -0300

    refs https://github.com/adminfaces/admin-theme/issues/42

--- src/main/java/com/github/adminfaces/showcase/bean/UserLoginMB.java
+++ src/main/java/com/github/adminfaces/showcase/bean/UserLoginMB.java
@@ -20,16 +20,15 @@ import org.primefaces.context.RequestContext;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 import java.io.Serializable;
 
 @Named
 @ViewScoped
 public class UserLoginMB implements Serializable {
-    
+
     private String username;
-    
+
     private String password;
 
     public String getUsername() {
@@ -47,12 +46,12 @@ public class UserLoginMB implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
-  
-    public void login(ActionEvent event) {
+
+    public void login() {
         RequestContext context = RequestContext.getCurrentInstance();
         FacesMessage message = null;
         boolean loggedIn = false;
-        
+
         if(username != null && username.equals("admin") && password != null && password.equals("admin")) {
             loggedIn = true;
             message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
@@ -60,8 +59,8 @@ public class UserLoginMB implements Serializable {
             loggedIn = false;
             message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
         }
-        
+
         FacesContext.getCurrentInstance().addMessage(null, message);
         context.addCallbackParam("loggedIn", loggedIn);
-    }   
+    }
 }
