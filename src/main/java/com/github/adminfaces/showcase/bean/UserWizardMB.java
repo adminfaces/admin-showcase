344bb75ed95468b49b75ee7cffa6ce6d965d5d6c
rmpestano <rmpestano@gmail.com>
2017-10-13 18:19:25 -0300

    refs https://github.com/adminfaces/admin-theme/issues/91

---
+++ src/main/java/com/github/adminfaces/showcase/bean/UserWizardMB.java
@@ -0,0 +1,49 @@
+package com.github.adminfaces.showcase.bean;
+
+import com.github.adminfaces.showcase.model.User;
+import org.omnifaces.cdi.ViewScoped;
+import org.primefaces.event.FlowEvent;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class UserWizardMB implements Serializable {
+
+    private User user = new User();
+
+    private boolean skip;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void save() {
+        FacesMessage msg = new FacesMessage("Successful", "Welcome :" + user.getFirstname());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public boolean isSkip() {
+        return skip;
+    }
+
+    public void setSkip(boolean skip) {
+        this.skip = skip;
+    }
+
+    public String onFlowProcess(FlowEvent event) {
+        if (skip) {
+            skip = false;   //reset in case user goes back
+            return "confirm";
+        } else {
+            return event.getNewStep();
+        }
+    }
+}
---
+++ src/main/java/com/github/adminfaces/showcase/model/User.java
@@ -0,0 +1,96 @@
+package com.github.adminfaces.showcase.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String firstname;
+
+    private String lastname;
+
+    private Integer age;
+
+    private String street;
+
+    private String city;
+
+    private String postalCode;
+
+    private String info;
+
+    private String email;
+
+    private String phone;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
