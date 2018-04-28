f81f147c65542ad42379f2adc4af828fb27a3b09
rmpestano <rmpestano@gmail.com>
2018-04-28 16:42:21 -0300

    refs https://github.com/adminfaces/admin-theme/issues/161 refs https://github.com/adminfaces/admin-theme/issues/153 https://github.com/adminfaces/admin-template/issues/60

---
+++ src/main/java/com/github/adminfaces/showcase/bean/ShowcaseLayoutMB.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.adminfaces.showcase.bean;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Specializes;
+
+/**
+ *
+ * @author rmpestano
+ */
+@Specializes
+@SessionScoped
+public class ShowcaseLayoutMB extends com.github.adminfaces.template.bean.LayoutMB {
+    
+    private boolean flat;
+    
+    private boolean materialButtons;
+    
+    @PostConstruct
+    public void init() {
+        super.init();
+        flat = false;
+    }
+
+    public boolean isFlat() {
+        return flat;
+    }
+
+    public void setFlat(boolean flat) {
+        this.flat = flat;
+    }
+
+    public boolean isMaterialButtons() {
+        return materialButtons;
+    }
+
+    public void setMaterialButtons(boolean materialButtons) {
+        this.materialButtons = materialButtons;
+    }
+    
+    
+    
+    
+    
+}
