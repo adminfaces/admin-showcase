5a3add79727bbcbcb138784f8f848a6b86f80902
rmpestano <rmpestano@gmail.com>
2018-07-31 22:25:59 -0300

    perf

--- src/test/java/com/github/adminfaces/showcase/pages/DataTablePage.java
+++ src/test/java/com/github/adminfaces/showcase/pages/DataTablePage.java
@@ -11,7 +11,6 @@ import static org.jboss.arquillian.graphene.Graphene.*;
 import org.jboss.arquillian.graphene.GrapheneElement;
 import org.jboss.arquillian.graphene.findby.FindByJQuery;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
@@ -23,34 +22,35 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class DataTablePage extends BasePage {
 
-	@FindByJQuery("hr.ui-separator + div.ui-datatable")
-	private GrapheneElement datatable;
+    @FindByJQuery("hr.ui-separator + div.ui-datatable")
+    private GrapheneElement datatable;
 
-	@FindByJQuery("div.ui-datatable-sticky tbody tr")
-	private List<GrapheneElement> datatableRows;
+    @FindByJQuery("div.ui-datatable-sticky td .ui-chkbox-box")
+    private List<GrapheneElement> datatableCheckBoxes;
 
-	@FindByJQuery(".ui-column-customfilter .ui-selectcheckboxmenu-trigger")
-	private GrapheneElement colorsFilterTrigger;
+    @FindByJQuery(".ui-column-customfilter .ui-selectcheckboxmenu-trigger")
+    private GrapheneElement colorsFilterTrigger;
 
-	public void filterByColor() {
-		guardNoRequest(colorsFilterTrigger).click();
-		List<WebElement> colorOptions = browser.findElements(By.cssSelector("div.ui-selectcheckboxmenu-items-wrapper ul li"));
-		waitModel().until().element(By.cssSelector("div.ui-selectcheckboxmenu-items-wrapper")).is().present();
-		for (int i = 0; i <= 1; i++) {
-			guardAjax(colorOptions.get(i)).click();
-		}
-	}
+    public void filterByColor() {
+        colorsFilterTrigger.click();
+        List<WebElement> colorOptions = browser.findElements(By.cssSelector("div.ui-selectcheckboxmenu-items-wrapper ul li"));
+        waitModel().until().element(By.cssSelector("div.ui-selectcheckboxmenu-items-wrapper")).is().present();
+        for (int i = 0; i <= 1; i++) {
+            guardAjax(colorOptions.get(i)).click();
+        }
+        browser.findElement(By.cssSelector("a.ui-selectcheckboxmenu-close .ui-icon-circle-close")).click();
+        waitModel().until().element(By.cssSelector("div.ui-selectcheckboxmenu-items-wrapper")).is().not().visible();
+        
+    }
 
-	public void selectRows() {
-		Actions actions = new Actions(browser);
-		actions.keyDown(Keys.LEFT_CONTROL);
-		for (int i = 0; i <= 2; i++) {
-			guardNoRequest(actions.moveToElement(datatableRows.get(i)).click()).perform();
-			waitModel();
-		}
-		actions.keyUp(Keys.LEFT_CONTROL);
-		assertThat(browser.findElements(By.cssSelector("tr.ui-datatable-selectable.ui-state-highlight")).size())
-				.isEqualTo(3);
-	}
+    public void selectRows() {
+        Actions actions = new Actions(browser);
+        for (int i = 0; i <= 2; i++) {
+            datatableCheckBoxes.get(i).click();
+            waitModel();
+        }
+        assertThat(browser.findElements(By.cssSelector("tr.ui-datatable-selectable.ui-state-highlight")).size())
+                .isEqualTo(3);
+    }
 
 }
